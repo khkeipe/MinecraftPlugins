3146ff95280c6e6afc41e9a566c339e158d72b35
khkeipe <kkeipe@gmail.com>
2020-09-22 21:30:50 -0500

    refactor plugin to use hasPlayed() method and added crafting recipe for book replacement

--- OutcraftTutuorial/src/me/clydescreations/OutcraftTutorial/Main.java
+++ OutcraftTutuorial/src/me/clydescreations/OutcraftTutorial/Main.java
@@ -3,8 +3,7 @@ package me.clydescreations.OutcraftTutorial;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,10 +11,10 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,34 +32,23 @@ public class Main extends JavaPlugin implements Listener {
         super.onEnable();
         saveDefaultConfig();
         this.getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.addRecipe(getRecipe());
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-            @Override
-            public void run() {
-                List<String> players = playerConfig.getStringList("BOOK_GIVEN");
-                String welcomMessage = getConfig().getString("WELCOME.MESSAGE").replace("[","").replace("]","").replace("{player}", event.getPlayer().getDisplayName());
 
-                if(!players.contains(event.getPlayer().getUniqueId().toString())){
-                    createGUI(event.getPlayer().getUniqueId());
-                    Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', welcomMessage));
-                }
+        Player p = event.getPlayer();
 
-                if(!playerList.exists()){
-                    try{
-                        playerList.createNewFile();
-                    }catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }, 5L);
+        boolean hasPlayed = p.hasPlayedBefore();
+
+        String welcomMessage = getConfig().getString("WELCOME.MESSAGE").replace("[","").replace("]","").replace("{player}", event.getPlayer().getDisplayName());
+
+        if(!hasPlayed) {
+            createGUI(p);
+            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', welcomMessage));
+        }
     }
-
-    private static File playerList = new File("plugins/OutCraft_Tutorial/players.yml");
-    private static FileConfiguration playerConfig = YamlConfiguration.loadConfiguration(playerList);
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent click){
@@ -70,35 +58,29 @@ public class Main extends JavaPlugin implements Listener {
 
         Player player = (Player) click.getWhoClicked();
 
-        List<String> players = playerConfig.getStringList("BOOK_GIVEN");
-
-        if(!players.contains(player.getUniqueId().toString())) {
-            players.add(player.getUniqueId().toString());
-            playerConfig.addDefault("BOOK_GIVEN", players);
             try {
-                playerConfig.options().copyDefaults(true);
-                playerConfig.save(playerList);
+                if(player.getInventory().firstEmpty() == -1) {
+                    player.getWorld().dropItemNaturally(player.getLocation(),getBook());
+                }else {
+                    player.getInventory().addItem(getBook());
+                }
                 player.getOpenInventory().close();
-                player.updateInventory();
-                player.getWorld().dropItemNaturally(player.getLocation(),book);
                 click.setCancelled(true);
             } catch (Exception e) {
                 e.printStackTrace();
+                player.sendMessage( "" + ChatColor.RED + "Oops, something went wrong. Please contact the devs!");
             }
-        }
     }
 
-    private  ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
 
-    public void createGUI(UUID playerID) {
 
-        String guiTitle = getConfig().get("GUI.TITLE").toString().replace("[","").replace("]","");
+    private ItemStack getBook(){
+        ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+
         String bookTitle = getConfig().get("BOOK.TITLE").toString().replace("[","").replace("]","");;
         String bookAuthor = getConfig().get("BOOK.AUTHOR").toString().replace("[","").replace("]","");;
         List<String> bookPages = getConfig().getStringList("BOOK.PAGES");
         bookPages.forEach(page -> ChatColor.translateAlternateColorCodes('&', page).replace("[","").replace("]",""));
-
-        gui = Bukkit.createInventory(null, 9, ChatColor.translateAlternateColorCodes('&', guiTitle));
 
         BookMeta bookMeta = (BookMeta) book.getItemMeta();
 
@@ -113,18 +95,36 @@ public class Main extends JavaPlugin implements Listener {
 
         book.setItemMeta(bookMeta);
 
+        return book;
+    }
+
+
+    public void createGUI(Player player) {
+
         getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
             @Override
             public void run() {
-                gui.setItem(4, book);
-
-                Player player = Bukkit.getPlayer(playerID);
+                String guiTitle = getConfig().get("GUI.TITLE").toString().replace("[","").replace("]","");
+                gui = Bukkit.createInventory(null, 9, ChatColor.translateAlternateColorCodes('&', guiTitle));
+                gui.setItem(4, getBook());
 
                 if (player != null) {
                     player.openInventory(gui);
                 }
             }
-        },1L);
+        },5L);
 
+    }
+
+    public ShapelessRecipe getRecipe() {
+
+        NamespacedKey key = new NamespacedKey(this, "tutorial_book");
+
+        ShapelessRecipe recipe = new ShapelessRecipe(key, getBook());
+
+        recipe.addIngredient(Material.PURPLE_CARPET);
+        recipe.addIngredient(Material.BOOK);
+
+        return recipe;
     }
 }
